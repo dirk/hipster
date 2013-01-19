d1a37862e4d019120672c59c74f32903109d7510
Thomas Cannon <tcannon@codemonkeydev.com>
2013-01-18 21:32:39 -0500

    added the autorun include and a configuration option to have tests run randomly to remove dependencies

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,9 +2,17 @@
 
 require 'rubygems'
 require 'bundler/setup'
+require 'rspec/autorun'
 
 require 'hipster'
 
 RSpec.configure do |config|
   # some (optional) config here
+
+	# Run specs in random order to surface order dependencies. If you find an
+	# order dependency and want to debug it, you can fix the order by providing
+	# the seed, which is printed after each run.
+	#     --seed 1234
+	config.order = "random"
+
 end
