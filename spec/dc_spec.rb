02e9fe8ba6c11830227b0812be2b8894c6f48781
Thomas Cannon <tcannon@codemonkeydev.com>
2013-01-18 21:33:15 -0500

    named Hipster::DublinCore to its new class name Hipster::Object::DublinCore

--- spec/dc_spec.rb
+++ spec/dc_spec.rb
@@ -1,23 +1,23 @@
 require 'spec_helper'
 
-describe Hipster::DublinCore do
+describe Hipster::Object::DublinCore do
   before :all do
     # @terms = [
     #   'contributor', 'coverage', 'creator', 'date', 'description', 'format',
     #   'identifier', 'language', 'publisher', 'relation', 'rights', 'source',
     #   'subject', 'title', 'type'
     # ]
-    @terms = Hipster::DublinCore::TERMS
+    @terms = Hipster::Object::DublinCore::TERMS
   end
   
   it "should default terms to nil" do
-    dc = Hipster::DublinCore.new
+    dc = Hipster::Object::DublinCore.new
     @terms.each do |term|
       dc.send(term.to_sym).should be_nil
     end
   end
   it "should have empty meta" do
-    dc = Hipster::DublinCore.new
+    dc = Hipster::Object::DublinCore.new
     dc.meta.should == {}
   end
 end
